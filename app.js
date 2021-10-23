543912adf69f846719ebbe8b7d826eeb8f082940
mcalijr <martink.moc@gmail.com>
2021-10-23 22:06:47 +0200

    Added all HTML and CSS needed for implementation of game logic in CSS, all that's leftis JS and then styling that little game so it looks pleasant for the eyes

---
+++ app.js
@@ -0,0 +1,9 @@
+// add class X or O to div with class .board once it is turn of X or O player, so it will fire the hover effect
+
+// After game finishes, add .show class to div  with .winning-message, so the message will show.
+
+// After player clicks on any part of the board, add HIS element into it (so either O or X, depending on whose round it is)
+
+function popupToggle() {
+  console.log("hello");
+}
